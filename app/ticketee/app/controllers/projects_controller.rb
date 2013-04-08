466a898b492f9e552df301cb187fd112b966e0cb
Zachary Bryan <zachx1@gmail.com>
2013-04-07 21:51:45 -0700

    Creation of project feature complete

--- app/ticketee/app/controllers/projects_controller.rb
+++ app/ticketee/app/controllers/projects_controller.rb
@@ -1,6 +1,25 @@
 class ProjectsController < ApplicationController
+	
 	def index
+
 	end
+
 	def new
+		@project = Project.new
 	end
+
+	def create
+		if @project = Project.new(params[:project]) 
+			flash[:notice] = "Project has been created."
+			redirect_to @project
+		else
+			flash[:alert] = "Project has not been created."
+      render :action => "new"
+		end
+	end
+
+	def show
+		@project = Project.find(params[:id])
+	end
+
 end
