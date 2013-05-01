dce8b0224e7953a50b6fed9df119f4e6ce402901
Zachary Bryan <zachx1@gmail.com>
2013-04-30 20:18:45 -0700

    Add feature for ensuring that a user is signed in via a confirmation email by using email spec

--- app/ticketee/spec/integration/signing_up_spec.rb
+++ app/ticketee/spec/integration/signing_up_spec.rb
@@ -7,7 +7,8 @@ feature 'Signing up' do
     fill_in "Email", with: "user@ticketee.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
-     click_button "Sign up"
-    page.should have_content("You have signed up successfully.")
+    click_button "Sign up"
+    message = "Please open the link to activate your account."
+		page.should have_content(message)
   end
 end
